e8c62e0b68fbd286d70afbe40f2363852f872cef
Andrés Pereira de Lucena <apereira@alabs.org>
2018-01-16 15:02:52 +0100

    Fixes deploy with capistrano (database.yml symlink)

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.8.0"
 
 set :application, "decidim"
 set :repo_url, "https://github.com/AjMalgrat/decidim-malgrat.git"
-set :linked_files, fetch(:linked_files, []).push(".rbenv-vars")
+set :linked_files, fetch(:linked_files, []).push("config/database.yml", ".rbenv-vars")
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/cache", "public/uploads")
 set :rbenv_type, :user
 set :rbenv_ruby, "2.4.2"
