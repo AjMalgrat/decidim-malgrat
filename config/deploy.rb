7c95579cad7170a907cf9a9305947e08faf6e93d
Mijail Rondon <mijail@riseup.net>
2018-10-22 17:04:54 +0200

    Add puma binds and rbenv version

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,10 +9,11 @@ set :deploy_to, "/home/ruby-data/app"
 
 set :rbenv_type, :user
 set :delayed_job_workers, 1
+set :rbenv_ruby, '2.5.1'
 
 append :linked_files, "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 set :puma_bind, "tcp://0.0.0.0:3000"
 set :puma_user, fetch(:user)
-
+append :rbenv_map_bins, 'puma', 'pumactl'
